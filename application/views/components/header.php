66b681ffc55d6534f566f194b8b27ae47ce3eb5a
Tjoosten <Tjoosten3@gmail.com>
2015-03-26 09:01:38 +0100

    add css links

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>C &amp; C | <?php echo $title; ?></title>
+    <title>C &amp; C RPi | <?php echo $title; ?></title>
+
+    <style rel="stylesheet" href="" />
+    <style rel="stylesheet" href="/assets/css/costum.css" />
   </head>
   <body>
