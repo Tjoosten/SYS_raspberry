5d79ef0f0acec4a587ea36aef26ca467ba70a814
Tjoosten <Tjoosten3@gmail.com>
2015-03-26 09:02:20 +0100

    fix

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -3,7 +3,7 @@
   <head>
     <title>C &amp; C RPi | <?php echo $title; ?></title>
 
-    <style rel="stylesheet" href="" />
-    <style rel="stylesheet" href="/assets/css/costum.css" />
+    <link rel="stylesheet" href="" />
+    <link rel="stylesheet" href="/assets/css/costum.css" />
   </head>
   <body>
